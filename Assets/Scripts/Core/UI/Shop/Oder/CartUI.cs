2cab64d2851eb832d8f6b3fc7b8034509421bdba
nhokty285 <147700656+nhokty285@users.noreply.github.com>
2026-04-20 15:17:28 +0700

    backup 20-Apr-26 15:17:28.15

--- Assets/Scripts/Core/UI/Shop/Oder/CartUI.cs
+++ Assets/Scripts/Core/UI/Shop/Oder/CartUI.cs
@@ -73,8 +73,6 @@ public class CartUI : MonoBehaviour
     [SerializeField] private RectTransform selectZone;
     [SerializeField] public bool isSelectMode = false;     // 🆕 THÊM: track chế độ chọn thủ công
 
-    public int number;
-
     private void Start()
     {
         SetupEventListeners();
